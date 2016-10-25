ff66832f462ea6d125e0f9295df52b58ff56d837
borysl <borys.lebeda@gmail.com>
2016-10-26 01:27:24 +0300

    Change color of the main content on hover mouse

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 
 var ele = document.getElementById("username");
 ele.innerHTML = "Borys Lebeda";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style.background = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.background = "";
+};
