2197cf653fbfaecd4ab05ebdb8a1294abc35e644
borysl <borys.lebeda@gmail.com>
2016-10-26 02:09:41 +0300

    Change text when it is hide or shown

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -23,5 +23,10 @@
 
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show sidebar");
+        } else {
+            $(this).text("Hide sidebar");
+        }
     });
 })();
