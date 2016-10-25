01a1998f80e3923fe25c86094ddfb0dbfe04db7e
borysl <borys.lebeda@gmail.com>
2016-10-26 01:32:46 +0300

    Wrap code in startup function

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,15 @@
 ﻿// site.js
 
-var ele = document.getElementById("username");
-ele.innerHTML = "Borys Lebeda";
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Borys Lebeda";
 
-var main = document.getElementById("main");
-main.onmouseenter = function () {
-    main.style.background = "#888";
-};
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.background = "#888";
+    };
 
-main.onmouseleave = function () {
-    main.style.background = "";
-};
+    main.onmouseleave = function () {
+        main.style.background = "";
+    };
+})();
