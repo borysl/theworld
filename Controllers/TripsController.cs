6af10f3ef1b5f5586f7801e10d20bcb774349c0b
borysl <borys.lebeda@gmail.com>
2016-11-30 00:56:54 +0200

    Add API controller

---
+++ Controllers/TripsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers
+{
+    public class TripsController : Controller
+    {
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            if (true) return BadRequest("Bad request");
+            return Ok(new Trip {Name = "My Trip"});
+        }
+    }
+}
