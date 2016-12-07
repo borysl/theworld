ef4c4b4bc30d8907b5f34acab551a8575328ba03
borysl <borys.lebeda@gmail.com>
2016-12-08 01:09:17 +0200

    Implement handler for the credentials

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers
 {
     public class AuthController : Controller
     {
+        private const bool DONT_LOCKOUT_ON_FAILURE = false;
+        private const bool PERSIST_LOGIN = true;
+
+        private readonly SignInManager<WorldUser> _signInManager;
+
+        public AuthController(SignInManager<WorldUser> signInManager )
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
@@ -15,6 +28,32 @@ namespace TheWorld.Controllers
                 return RedirectToAction("Trips", "Home");
             }
 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, PERSIST_LOGIN, DONT_LOCKOUT_ON_FAILURE);
+
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Trips", "Home");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password incorrect.");
+                }
+            }
             return View();
         }
     }
